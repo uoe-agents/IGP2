Added plotting for maps; fixed bugs with boundary calculation

igp2/opendrive/map.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 
 from typing import Union, Tuple, List
 from datetime import datetime
@@ -13,33 +15,38 @@ from igp2.opendrive.parser import parse_opendrive
 
 class Map(object):
     """ Define a map object based on the OpenDrive standard """
+
     def __init__(self, opendrive: OpenDrive = None):
         """ Create a map object given the parsed OpenDrive file
 
         Args:
             opendrive: A class describing the parsed contents of the OpenDrive file
         """
-        self.opendrive = opendrive
+        self.__opendrive = opendrive
 
         self.__process_header()
         self.__process_road_layout()
 
     def __process_header(self):
-        self.name = self.opendrive.header.name
-        self.date = datetime.strptime(self.opendrive.header.date, "%c")
-        self.geo_reference = self.opendrive.header.geo_reference
+        self.__name = self.__opendrive.header.name
+        # self.__date = datetime.strptime(self.__opendrive.header.date, "%c")
+        self.__north = float(self.__opendrive.header.north)
+        self.__west = float(self.__opendrive.header.west)
+        self.__south = float(self.__opendrive.header.south)
+        self.__east = float(self.__opendrive.header.east)
+        self.__geo_reference = self.__opendrive.header.geo_reference
 
     def __process_road_layout(self):
         roads = {}
 
-        for road in self.opendrive.roads:
+        for road in self.__opendrive.roads:
             road.plan_view.precalculate(linestring=True)
             road.calculate_boundary()
 
             assert road.id not in roads
             roads[road.id] = road
 
-        self.roads = roads
+        self.__roads = roads
 
     def roads_at(self, point: Union[Point, Tuple[float, float], np.ndarray]) -> List[Road]:
         """ Find all roads that pass through the given point
@@ -55,13 +62,76 @@ class Map(object):
         for id, road in self.roads.items():
             if road.boundary.contains(point):
                 candidates.append(road)
-        return road
+        return candidates
 
     def lanes_at(self, point: Union[Point, Tuple[float, float], np.ndarray]) -> List[Lane]:
         pass
 
     def best_lane_at(self, point: Union[Point, Tuple[float, float], np.ndarray], angle: float) -> Lane:
         pass
+
+    def plot(self, midline: bool = True, ax: plt.Axes = None, **kwargs) -> plt.Axes:
+        """ Draw the road layout of the map
+
+        Args:
+            ax: Axes to draw on
+            midline: True if the midline of roads should be drawn
+
+        Keyword Args:
+            road_color: Plot color of the road boundary (default: black)
+            midline_color: Color of the midline (default: red)
+
+        Returns:
+            The axes onto which the road layout was drawn
+        """
+        if ax is None:
+            _, ax = plt.subplots(1, 1)
+
+        ax.set_xlim([self.west, self.east])
+        ax.set_ylim([self.south, self.north])
+
+        for road_id, road in self.roads.items():
+            ax.plot(road.boundary.boundary.xy[0], road.boundary.boundary.xy[1],
+                    color=kwargs.get("road_color", "k"))
+
+            if midline:
+                ax.plot(road.midline.xy[0], road.midline.xy[1],
+                        color=kwargs.get("midline_color", "r"),
+                        )
+
+        return ax
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def date(self):
+        return self.__date
+
+    @property
+    def geo_reference(self):
+        return self.__geo_reference
+
+    @property
+    def roads(self):
+        return self.__roads
+
+    @property
+    def north(self):
+        return self.__north
+
+    @property
+    def south(self):
+        return self.__south
+
+    @property
+    def east(self):
+        return self.__east
+
+    @property
+    def west(self):
+        return self.__west
 
     @classmethod
     def parse_from_opendrive(cls, file_path: str):
@@ -79,5 +149,6 @@ class Map(object):
 
 
 if __name__ == '__main__':
-    map = Map.parse_from_opendrive("scenarios/frankenberg.xodr")
-    roads = map.roads_at((66.1, -16.25))
+    map = Map.parse_from_opendrive("scenarios/heckstrasse.xodr")
+    map.plot()
+    plt.show()