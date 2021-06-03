Fix for index error in heading calculation

igp2/recognition/astar.py
@@ -120,8 +120,7 @@ class AStar:
         direction = np.array([np.cos(heading), np.sin(heading)])
         point = trajectory.path[-1] + self.next_lane_offset * direction
         velocity = trajectory.velocity[-1]
-        offset_trajectory = VelocityTrajectory(np.array([point]), np.array([velocity]))
-        trajectory.extend(offset_trajectory)
+        trajectory.extend((np.array([point]), np.array([velocity])))
 
     def _full_trajectory(self, macro_actions: List[MacroAction]):
         path = np.empty((0, 2), float)

igp2/trajectory.py
@@ -131,7 +131,7 @@ class Trajectory(abc.ABC):
     def trajectory_times(self):
         return np.cumsum(self.trajectory_dt())
 
-    def extend(self, new_trajectory: "Trajectory"):
+    def extend(self, new_trajectory):
         raise NotImplementedError
 
 
@@ -260,9 +260,13 @@ class VelocityTrajectory(Trajectory):
         path_lengths = np.linalg.norm(np.diff(path, axis=0), axis=1)  # Length between points
         return np.cumsum(np.append(0, path_lengths))
 
-    def extend(self, new_trajectory: "Trajectory"):
-        self._path = np.concatenate([self.path, new_trajectory.path], axis=0)
-        self._velocity = np.concatenate([self.velocity, new_trajectory.velocity])
+    def extend(self, new_trajectory):
+        if isinstance(new_trajectory, Trajectory):
+            self._path = np.concatenate([self.path, new_trajectory.path], axis=0)
+            self._velocity = np.concatenate([self.velocity, new_trajectory.velocity])
+        else:
+            self._path = np.concatenate([self.path, new_trajectory[0]], axis=0)
+            self._velocity = np.concatenate([self.velocity, new_trajectory[1]])
 
 
 class VelocitySmoother: