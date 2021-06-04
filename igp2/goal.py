70b17e03f7bcc30f7d9cc53b8b71b4838e6c9950
Gyevnar Balint <gyevnarb@gmail.com>
2021-06-04 12:02:01 +0200

    Added string representation to Goals

--- igp2/goal.py
+++ igp2/goal.py
@@ -29,6 +29,9 @@ class PointGoal(Goal):
         self._radius = threshold
         self._center = np.array([self._point.x, self._point.y]) if isinstance(point, Point) else point
 
+    def __repr__(self):
+        return f"PointGoal(center={self._center}, r={self._radius})"
+
     def reached(self, point: Point) -> bool:
         coord = np.array([point.x, point.y])
         diff = np.subtract(self._center, coord)
@@ -40,6 +43,9 @@ class BoxGoal(Goal):
         super().__init__()
         self._poly = Polygon(box.boundary)
         self._center = box.center
+
+    def __repr__(self):
+        return f"BoxGoal(center={self._center}, bounds={list(self._poly.coords)})"
 
     def reached(self, point: Point) -> bool:
         return self._poly.contains(point) or self._poly.touches(point)
