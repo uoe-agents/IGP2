53fe805f1ee66c0395e69bce496189a9919df88c
Cillian Brewitt <cillianbrewitt@gmail.com>
2021-06-08 18:33:34 +0100

    allow switch lane maneuver to handle following lane sections

--- igp2/planlibrary/maneuver.py
+++ igp2/planlibrary/maneuver.py
@@ -383,12 +383,26 @@ class SwitchLane(Maneuver, ABC):
         current_lane = scenario_map.best_lane_at(state.position, state.heading)
         target_lane = scenario_map.best_lane_at(self.config.termination_point, drivable_only=True)
 
-        assert current_lane.parent_road == target_lane.parent_road, 'initial lane and target lane not in same road'
-        assert abs(current_lane.id - target_lane.id) == 1, 'initial lane and target lane not adjacent'  # TODO: This could fail incorrectly, when a road merges into another
+        assert target_lane in self.get_possible_target_lanes(current_lane)
 
         path = self._get_path(state, target_lane)
         velocity = self.get_velocity(path, agent_id, frame, [target_lane])
         return VelocityTrajectory(path, velocity)
+
+    @classmethod
+    def _get_possible_target_lanes(cls, current_lane: Lane) -> List[Lane]:
+        possible_lanes = []
+        distance = -current_lane.length
+        while distance <= cls.TARGET_SWITCH_LENGTH:
+            distance += current_lane.length
+            for target_lane in current_lane.lane_section.all_lanes:
+                if abs(current_lane.id - target_lane.id) == 1:
+                    possible_lanes.append(target_lane)
+            if len(current_lane.link.successor) == 1:
+                current_lane = current_lane.link.successor[0]
+            else:
+                return possible_lanes
+        return possible_lanes
 
 
 class SwitchLaneLeft(SwitchLane):
