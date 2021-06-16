be9c2b3cdfd16d2b9e35c4f0ca4a253c6e2d7e88
francelico <garcin.samuel@gmail.com>
2021-06-16 15:24:32 +0100

    feat: fixed SLURM script

---
+++ scripts/experiments/SLURM_experiment.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=IGP2_GRIT_cost_tuning_angular_velocity
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=128
+#SBATCH --time=12:00:00
+#SBATCH --mem-per-cpu=3G
+
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate IGP2
+export PYTHONPATH=$HOME/igp2-dev
+python ~/igp2-dev/scripts/experiments/experiment_multi_process.py --num_workers 128 --output angular_velocity_tuning
