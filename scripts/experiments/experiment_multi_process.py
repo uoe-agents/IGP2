19c3fb53e6b1b8796136f57fe53fce010e149e65
Samuel Garcin <garcin.samuel@gmail.com>
2021-07-29 17:04:06 +0100

    tuning: checking heading 1000 once more

--- scripts/experiments/experiment_multi_process.py
+++ scripts/experiments/experiment_multi_process.py
@@ -225,7 +225,7 @@ if __name__ == '__main__':
 
     if TUNING:
         cost_factors_arr = []
-        cost_factors_arr.append({"time": 0.0, "velocity": 0.0, "acceleration": 0.0, "jerk": 0., "heading":1000, "angular_velocity": 0.0,
+        cost_factors_arr.append({"time": 0.001, "velocity": 0.0, "acceleration": 0.0, "jerk": 0., "heading":1000, "angular_velocity": 0.0,
                             "angular_acceleration": 0., "curvature": 0.0, "safety": 0.})
         # cost_factors_arr.append({"time": 0.001, "velocity": 0.0, "acceleration": 0.0, "jerk": 0., "heading":0.1, "angular_velocity": 0.0,
         #                     "angular_acceleration": 0., "curvature": 0.0, "safety": 0.})
