3deb2d898825143e3b5a86785d3cd90e8fb28a89
Samuel Garcin <garcin.samuel@gmail.com>
2021-07-29 16:13:38 +0100

    tuning: 0 time factor to check if issue is coming from there

--- scripts/experiments/experiment_multi_process.py
+++ scripts/experiments/experiment_multi_process.py
@@ -225,16 +225,16 @@ if __name__ == '__main__':
 
     if TUNING:
         cost_factors_arr = []
-        cost_factors_arr.append({"time": 0.001, "velocity": 0.0, "acceleration": 0.0, "jerk": 0., "heading":0.01, "angular_velocity": 0.0,
+        cost_factors_arr.append({"time": 0.0, "velocity": 0.0, "acceleration": 0.0, "jerk": 0., "heading":1000, "angular_velocity": 0.0,
                             "angular_acceleration": 0., "curvature": 0.0, "safety": 0.})
-        cost_factors_arr.append({"time": 0.001, "velocity": 0.0, "acceleration": 0.0, "jerk": 0., "heading":0.1, "angular_velocity": 0.0,
-                            "angular_acceleration": 0., "curvature": 0.0, "safety": 0.})
-        cost_factors_arr.append({"time": 0.001, "velocity": 0.0, "acceleration": 0.0, "jerk": 0., "heading":1, "angular_velocity": 0.0,
-                            "angular_acceleration": 0., "curvature": 0.0, "safety": 0.})
-        cost_factors_arr.append({"time": 0.001, "velocity": 0.0, "acceleration": 0.0, "jerk": 0., "heading":10, "angular_velocity": 0.0,
-                            "angular_acceleration": 0., "curvature": 0.0, "safety": 0.})
-        cost_factors_arr.append({"time": 0.001, "velocity": 0.0, "acceleration": 0.0, "jerk": 0., "heading":100, "angular_velocity": 0.0,
-                            "angular_acceleration": 0., "curvature": 0.0, "safety": 0.})
+        # cost_factors_arr.append({"time": 0.001, "velocity": 0.0, "acceleration": 0.0, "jerk": 0., "heading":0.1, "angular_velocity": 0.0,
+        #                     "angular_acceleration": 0., "curvature": 0.0, "safety": 0.})
+        # cost_factors_arr.append({"time": 0.001, "velocity": 0.0, "acceleration": 0.0, "jerk": 0., "heading":1, "angular_velocity": 0.0,
+        #                     "angular_acceleration": 0., "curvature": 0.0, "safety": 0.})
+        # cost_factors_arr.append({"time": 0.001, "velocity": 0.0, "acceleration": 0.0, "jerk": 0., "heading":10, "angular_velocity": 0.0,
+        #                     "angular_acceleration": 0., "curvature": 0.0, "safety": 0.})
+        # cost_factors_arr.append({"time": 0.001, "velocity": 0.0, "acceleration": 0.0, "jerk": 0., "heading":100, "angular_velocity": 0.0,
+        #                     "angular_acceleration": 0., "curvature": 0.0, "safety": 0.})
         for idx, cost_factors in enumerate(cost_factors_arr):
             logger.info(f"Starting experiment {idx} with cost factors {cost_factors}.")
             t_start = time.perf_counter()
