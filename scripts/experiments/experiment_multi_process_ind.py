27635792ff61bd934cd299b4ca310f5eec2953dc
Samuel Garcin <garcin.samuel@gmail.com>
2021-07-30 17:51:12 +0100

    tuning: ready to run ind testset

--- scripts/experiments/experiment_multi_process_ind.py
+++ scripts/experiments/experiment_multi_process_ind.py
@@ -227,14 +227,6 @@ if __name__ == '__main__':
         cost_factors_arr = []
         cost_factors_arr.append({"time": 0.0, "velocity": 0, "acceleration": 0.1, "jerk": 0., "heading":1000, "angular_velocity": 0.0,
                             "angular_acceleration": 0., "curvature": 0.0, "safety": 0.})
-        cost_factors_arr.append({"time": 0.0, "velocity": 0, "acceleration": 1, "jerk": 0., "heading":1000, "angular_velocity": 0.0,
-                            "angular_acceleration": 0., "curvature": 0.0, "safety": 0.})
-        cost_factors_arr.append({"time": 0.0, "velocity": 0, "acceleration": 10, "jerk": 0., "heading":1000, "angular_velocity": 0.0,
-                            "angular_acceleration": 0., "curvature": 0.0, "safety": 0.})
-        cost_factors_arr.append({"time": 0.0, "velocity": 0, "acceleration": 100, "jerk": 0., "heading":1000, "angular_velocity": 0.0,
-                            "angular_acceleration": 0., "curvature": 0.0, "safety": 0.})
-        cost_factors_arr.append({"time": 0.0, "velocity": 0, "acceleration": 1000, "jerk": 0., "heading":1000, "angular_velocity": 0.0,
-                            "angular_acceleration": 0., "curvature": 0.0, "safety": 0.})
 
         for idx, cost_factors in enumerate(cost_factors_arr):
             logger.info(f"Starting experiment {idx} with cost factors {cost_factors}.")
