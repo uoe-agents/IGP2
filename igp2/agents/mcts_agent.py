Added view radius check to MCTSAgent; Fixed trajectory maneuver lane sequence calculation

igp2/agents/mcts_agent.py
@@ -78,8 +78,11 @@ class MCTSAgent(MacroAgent):
         agents_metadata = AgentMetadata.default_meta_frame(frame)
         self._goal_probabilities = {aid: GoalsProbabilities(self._goals) for aid in frame.keys()}
         for agent_id in frame:
-            if agent_id == self.agent_id:
+            state = frame[agent_id]
+            agent_distance = np.linalg.norm(state.position - frame[self.agent_id].position)
+            if agent_id == self.agent_id or agent_distance >= self.view_radius:
                 continue
+
             self._goal_recognition.update_goals_probabilities(self._goal_probabilities[agent_id],
                                                               self._observations[agent_id][0],
                                                               agent_id, self._observations[agent_id][1], frame, None)
@@ -93,7 +96,7 @@ class MCTSAgent(MacroAgent):
 
         if self._k >= self._kmax or self.current_macro is None or \
                 (self.current_macro.done(observation) and self._current_macro_id == len(self._macro_actions) - 1):
-            self.get_goals(observation)
+            self._goals = self.get_goals(observation)
             self.update_plan(observation)
             self.update_macro_action(self._macro_actions[0], observation)
             self._k = 0
@@ -123,13 +126,15 @@ class MCTSAgent(MacroAgent):
         for aid in list(self._observations.keys()):
             if aid not in frame: self._observations.pop(aid)
 
-    def get_goals(self, observation: Observation):
+    def get_goals(self, observation: Observation) -> List[Goal]:
         """Retrieve all possible goals reachable from the current position on the map in any direction. If more than
         one goal is found on a single lane, then only choose the one furthest along the midline of the lane.
 
         Args:
             observation: Observation of the environment
         """
+        # TODO Replace with box goals that cover all lanes in same direction
+
         scenario_map = observation.scenario_map
         state = observation.frame[self.agent_id]
         view_circle = Point(*state.position).buffer(self.view_radius)
@@ -168,7 +173,7 @@ class MCTSAgent(MacroAgent):
                             continue
                     possible_goals.append(PointGoal(np.array(new_point), threshold=2.0))
 
-        self._goals = possible_goals
+        return possible_goals
 
     def _advance_macro(self, observation: Observation):
 
@@ -197,3 +202,8 @@ class MCTSAgent(MacroAgent):
         corresponding to agents ids. It stores the trajectory observed so far and the frame
         at which each agent was initially observed. Currently, any agent out of view is immediately forgotten."""
         return self._observations
+
+    @property
+    def possible_goals(self) -> List[Goal]:
+        """ Return the current list of possible goals. """
+        return self._goals

igp2/recognition/astar.py
@@ -3,12 +3,14 @@ import traceback
 import numpy as np
 import heapq
 import logging
+import matplotlib.pyplot as plt
 from typing import Callable, List, Dict, Tuple
 
 from shapely.geometry import Point, LineString
 
+from igp2.opendrive.plot_map import plot_map
 from igp2.agents.agentstate import AgentState
-from igp2.goal import PointGoal
+from igp2.goal import PointGoal, Goal, BoxGoal
 from igp2.opendrive.map import Map
 from igp2.planlibrary.macro_action import MacroAction
 from igp2.planlibrary.maneuver import Maneuver
@@ -48,7 +50,8 @@ class AStar:
                scenario_map: Map,
                n_trajectories: int = 1,
                open_loop: bool = True,
-               current_maneuver: Maneuver = None) -> Tuple[List[VelocityTrajectory], List[List[MacroAction]]]:
+               current_maneuver: Maneuver = None,
+               debug: bool = False) -> Tuple[List[VelocityTrajectory], List[List[MacroAction]]]:
         """ Run A* search from the current frame to find trajectories to the given goal.
 
         Args:
@@ -59,6 +62,7 @@ class AStar:
             n_trajectories: The number of trajectories to return
             open_loop: Whether to generate open loop or closed loop macro actions in the end
             current_maneuver: The currently executed maneuver of the agent
+            debug: If True, then plot the evolution of the frontier at each step
 
         Returns:
             List of VelocityTrajectories ordered in increasing order of cost. The best trajectory is at index 0, while
@@ -91,6 +95,16 @@ class AStar:
             # Check if path has self-intersection
             if actions:  # and scenario_map.in_roundabout(frame[agent_id].position, frame[agent_id].heading):
                 if not LineString(trajectory.path).is_simple: continue
+
+                if debug:
+                    plot_map(scenario_map, markings=True)
+                    for aid, a in frame.items():
+                        plt.plot(*a.position, marker="o")
+                        plt.text(*a.position, aid)
+                    plt.plot(*list(zip(*trajectory.path)))
+                    plt.plot(*goal.center, marker="x")
+                    plt.title(f"agent {agent_id} -> {goal.center}: {actions}")
+                    plt.show()
 
             for macro_action in MacroAction.get_applicable_actions(frame[agent_id], scenario_map):
                 for ma_args in macro_action.get_possible_args(frame[agent_id], scenario_map, goal.center):
@@ -125,11 +139,17 @@ class AStar:
         return np.linalg.norm(trajectory.path[-1] - goal.center) / Maneuver.MAX_SPEED
 
     @staticmethod
-    def goal_reached(goal: PointGoal, trajectory: VelocityTrajectory) -> bool:
+    def goal_reached(goal: Goal, trajectory: VelocityTrajectory) -> bool:
         if trajectory is None:
             return False
-        distances = np.linalg.norm(trajectory.path - goal.center, axis=1)
-        return np.any(np.isclose(distances, 0.0, atol=goal.radius))
+
+        if isinstance(goal, PointGoal):
+            distances = np.linalg.norm(trajectory.path - goal.center, axis=1)
+            return np.any(np.isclose(distances, 0.0, atol=goal.radius))
+        elif isinstance(goal, BoxGoal):
+            return any([goal.reached(p) for p in trajectory.path])
+        else:
+            return False
 
     def _add_offset_point(self, trajectory):
         """ Add a small step at the end of the trajectory to reach within the boundary of the next lane. """

scripts/experiments/carla_mcts_scenario.py
@@ -14,6 +14,7 @@ from igp2.opendrive.map import Map
 from igp2.recognition.goalrecognition import GoalRecognition
 from igp2.recognition.goalprobabilities import GoalsProbabilities
 from igp2.recognition.astar import AStar
+from igp2.vehicle import Observation
 from igp2.velocitysmoother import VelocitySmoother
 from igp2.cost import Cost
 from igp2.opendrive.plot_map import plot_map
@@ -38,8 +39,8 @@ SCENARIOS = {
 
 heading = {0: -0.6,
            1: -0.6,
-           2: np.pi,
-           3: np.pi + 0.6,
+           2: np.deg2rad(120),
+           3: np.deg2rad(-160)
            }
 
 speed = {0: 1.5,
@@ -53,6 +54,7 @@ position = {0: np.array([6.0, 0.7]),
             2: np.array([73.2, -47.1]),
             3: np.array([61.7, -15.2]),
             }
+
 heckstrasse_frame ={}
 for aid in position.keys():
     heckstrasse_frame[aid] = AgentState(time=0,
@@ -73,9 +75,9 @@ scenario_map = SCENARIOS["heckstrasse"]
 frame = heckstrasse_frame
 goals = heckstrasse_goals
 ego_id = 2
-fps = 20
-T = 30.0
-carla_sim = CarlaSim(xodr='scenarios/maps/heckstrasse.xodr')
+fps = 20  # Simulator frequency
+T = 2  # MCTS update period
+carla_sim = CarlaSim(xodr='scenarios/maps/heckstrasse.xodr', carla_path="C:\\Users\\Balint\\Documents\\Agents\\Carla")
 
 # TODO: think of cleaner way
 goals_agents = {
@@ -84,15 +86,6 @@ goals_agents = {
     2: 0,  # ego goal
     3: 0,
 }
-
-# plot_map(scenario_map, markings=True)
-# for agent_id, state in frame.items():
-#     plt.plot(*state.position, marker="o")
-#     plt.text(*state.position, agent_id)
-# for goal_id, goal in goals.items():
-#     plt.plot(*goal.center, marker="x")
-#     plt.text(*goal.center, goal_id)
-# plt.show()
 
 # TODO Update
 cost_factors = {"time": 0.001, "velocity": 0.0, "acceleration": 0.0, "jerk": 0., "heading": 10, "angular_velocity": 0.0,
@@ -143,6 +136,17 @@ if __name__ == '__main__':
             agents[aid].set_trajectory(trajectory)
 
         carla_sim.add_agent(agents[aid])
+
+    # plot_map(scenario_map, markings=True)
+    # for agent_id, state in frame.items():
+    #     plt.plot(*state.position, marker="o")
+    #     plt.text(*state.position, agent_id)
+    # for i, goal in enumerate(agents[ego_id].get_goals(Observation(frame, scenario_map))):
+    #     plt.plot(*goal.center, marker="x")
+    #     plt.text(*goal.center, i)
+    # circle = plt.Circle(tuple(agents[ego_id].state.position), agents[ego_id].view_radius, color='r')
+    # plt.gca().add_patch(circle)
+    # plt.show()
 
     carla_sim.run()
 