Fix for center lane calculation; Added scenario 2 map

igp2/opendrive/elements/road.py
@@ -156,7 +156,7 @@ class Road:
                     reference_segment = start_segment
                     reference_widths = np.zeros_like(sample_distances)
 
-                lane_boundary, _, segment_widths = \
+                lane_boundary, segment_widths = \
                     lane.sample_geometry(sample_distances, reference_segment, reference_widths)
 
                 boundary = unary_union([boundary, lane_boundary])
@@ -213,7 +213,9 @@ class Road:
         # Assign midlines for each center lane for every lane section
         for ls in self.lanes.lane_sections:
             lane = ls.center_lanes[0]
-            lane._ref_line = substring(center_lane, ls.start_distance, ls.start_distance + ls.length)
+            lane._ref_line = substring(center_lane, ls.start_distance / ref_line.length,
+                                       (ls.start_distance + ls.length) / ref_line.length,
+                                       normalized=True)
 
     @property
     def boundary(self):

igp2/opendrive/elements/road_lanes.py
@@ -6,6 +6,8 @@ import numpy as np
 from shapely.geometry import Polygon, LineString, Point
 from dataclasses import dataclass
 
+from shapely.ops import unary_union
+
 from igp2.opendrive.elements.geometry import normalise_angle, ramer_douglas
 from igp2.opendrive.elements.road_record import RoadRecord
 
@@ -314,7 +316,7 @@ class Lane:
 
     def sample_geometry(self, sample_distances: np.ndarray,
                         reference_segment: LineString,
-                        reference_widths: np.ndarray) -> Tuple[Polygon, LineString, np.ndarray]:
+                        reference_widths: np.ndarray) -> Tuple[Polygon, np.ndarray]:
         """ Sample points of the lane boundary and midline.
 
         Args:
@@ -331,8 +333,7 @@ class Lane:
             self._boundary = Polygon()
             self._ref_line = reference_segment
             self._midline = reference_segment
-            return self._boundary, self._ref_line, np.zeros_like(reference_widths)
-
+            return self._boundary, np.zeros_like(reference_widths)
 
         direction = np.sign(self.id)
 
@@ -379,6 +380,13 @@ class Lane:
             buffer = Polygon(list(reference_segment.coords) + boundary_points[::-1])
             ref_line = LineString(boundary_points)
 
+        if not buffer.is_simple:
+            coords_list = []
+            for non_intersecting_ls in unary_union(buffer.boundary):
+                if non_intersecting_ls.length > 0.5:
+                    coords_list.extend(non_intersecting_ls.coords)
+            buffer = Polygon(coords_list)
+
         mid_line = LineString(ramer_douglas(midline_points[::skip], dist=0.05))
         if not mid_line.is_simple:
             mid_line = LineString(ramer_douglas(midline_points[::skip], dist=0.15))
@@ -387,7 +395,7 @@ class Lane:
         self._ref_line = ref_line
         self._midline = mid_line
 
-        return buffer, ref_line, widths
+        return buffer, widths
 
     def get_width_idx(self, width_idx) -> Optional[LaneWidth]:
         """ Get the LaneWidth object with the given index.