Fix cost calculation when at destination

igp2/cost.py
@@ -141,6 +141,10 @@ class Cost:
 
         trajectories_resampled = [self.resample_trajectory(trajectory, n) for trajectory in trajectories_resampled]
 
+        # handle case when we reach the end of the trajectory
+        if len(trajectories_resampled[0].path) == 1 or len(trajectories_resampled[1].path) == 1:
+            return 0.
+
         dcost_time_to_goal = self._d_time_to_goal(trajectories_resampled[0], trajectories_resampled[1])
         dcost_velocity = self._d_velocity(trajectories_resampled[0], trajectories_resampled[1])
         dcost_longitudinal_acceleration = self._d_longitudinal_acceleration(trajectories_resampled[0],
@@ -180,11 +184,15 @@ class Cost:
         u = trajectory_nostop.pathlength
         u_new = linspace(0, u[-1], n)
         k = min(k, len(velocity) - 1)
-        tck, _ = splprep([path[:, 0], path[:, 1], velocity, heading], u=u, k=k, s=0)
-        tck[0] = self.fix_points(tck[0])
-        path_new = np.empty((n, 2), float)
-        path_new[:, 0], path_new[:, 1], velocity_new, heading_new = splev(u_new, tck)
-        trajectory_resampled = ip.VelocityTrajectory(path_new, velocity_new, heading_new)
+
+        if k != 0:
+            tck, _ = splprep([path[:, 0], path[:, 1], velocity, heading], u=u, k=k, s=0)
+            tck[0] = self.fix_points(tck[0])
+            path_new = np.empty((n, 2), float)
+            path_new[:, 0], path_new[:, 1], velocity_new, heading_new = splev(u_new, tck)
+            trajectory_resampled = ip.VelocityTrajectory(path_new, velocity_new, heading_new)
+        else:
+            trajectory_resampled = trajectory
 
         return trajectory_resampled
 

scripts/experiments/carla_traffic_manager.py
@@ -13,17 +13,20 @@ def parse_args():
 
     config_specification.add_argument('--carla_path', default="/opt/carla-simulator",
                                       help="path to the directory where CARLA is installed", type=str)
-    config_specification.add_argument('--map', default="Town01",
+    config_specification.add_argument('--map', default="town01",
                                       help="name of the map (town) to use",
                                       type=str)
     config_specification.add_argument('--seed', default=None,
                                       help="random seed to use",
                                       type=int)
+    config_specification.add_argument('--no_rendering',
+                                      help="whether to disable CARLA rendering",
+                                      action="store_true")
     config_specification.add_argument('--record',
                                       help="whether to create an offline recording of the simulation",
                                       action="store_true")
 
-    config_specification.set_defaults(record=False)
+    config_specification.set_defaults(no_rendering=False, record=False)
     parsed_config_specification = vars(config_specification.parse_args())
     return parsed_config_specification
 
@@ -37,7 +40,7 @@ def main():
     ip.setup_logging()
     carla_path = config["carla_path"]
     scenario = config["map"]
-    xodr_path = f"scenarios/maps/{scenario.lower()}.xodr"
+    xodr_path = f"scenarios/maps/{scenario}.xodr"
 
     frame = {
         0: ip.AgentState(time=0,
@@ -47,7 +50,7 @@ def main():
                          heading=np.pi / 2)
     }
 
-    simulation = ip.carla.CarlaSim(map_name=scenario, xodr=xodr_path, carla_path=carla_path, rendering=True,
+    simulation = ip.carla.CarlaSim(map_name=scenario, xodr=xodr_path, carla_path=carla_path, rendering=not config["no_rendering"],
                                    record=config["record"])
 
     ego_id = 0