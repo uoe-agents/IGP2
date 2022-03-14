bf71f7ef95efc25545b5e3aa9dd9637bb3a5aa2f
Balint Gyevnar <gyevnarb@gmail.com>
2022-03-14 14:27:35 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(name='igp2',
-                 version='0.1',
-                 description='Reimplementation of IGP2 from the paper:'
-                             ' Interpretable Goal-based Prediction and Planning for Autonomous Driving',
+                 version='0.1.0',
+                 description='Open-source implementation of the goal recognition and motion planning algorithm IGP2 '
+                             'from the paper: Interpretable Goal-based Prediction and Planning for Autonomous Driving',
                  long_description=long_description,
                  long_description_content_type="text/markdown",
-                 author='Cillian Brewitt, Balint Gyevnar, Stefano Albrecht',
+                 author='Cillian Brewitt, Balint Gyevnar, Samuel Garcin, Massimiliano Tamborski, Stefano Albrecht',
                  author_email='cillian.brewitt@ed.ac.uk',
-                 url='https://github.com/cbrewitt/igp2-dev',
+                 url='https://github.com/uoe-agents/IGP2',
                  packages=setuptools.find_packages(exclude=["tests", "scripts"]),
                  install_requires=requirements
                  )
