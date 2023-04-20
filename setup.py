4114fbb556e905cd7298da4ccaa8ac4d0242fb23
Balint Gyevnar <gyevnarb@gmail.com>
2023-04-21 00:33:43 +0100

    Upgraded requirements to support Python 3.9

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(name='igp2',
-                 version='0.1.0',
+                 version='0.2.0',
                  description='Open-source implementation of the goal recognition and motion planning algorithm IGP2 '
                              'from the paper: Interpretable Goal-based Prediction and Planning for Autonomous Driving',
                  long_description=long_description,
                  long_description_content_type="text/markdown",
-                 author='Cillian Brewitt, Balint Gyevnar, Samuel Garcin, Massimiliano Tamborski, Stefano Albrecht',
-                 author_email='cillian.brewitt@ed.ac.uk',
+                 author='Balint Gyevnar, Cillian Brewitt, Samuel Garcin, Massimiliano Tamborski, Stefano Albrecht',
+                 author_email='balint.gyevnar@ed.ac.uk',
                  url='https://github.com/uoe-agents/IGP2',
                  packages=setuptools.find_packages(exclude=["tests", "scripts"]),
                  install_requires=requirements
